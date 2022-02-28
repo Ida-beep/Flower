7ceca8e7065f5e3b59372da941f3b1606dd72994
idavill <idamariavilladsen@gmail.com>
2022-02-28 15:38:55 +0100

    base structure and dailyFlower

--- flower/src/App.js
+++ flower/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Body from './components/Body';
 
 function App() {
+  const current = new Date();
+  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Body date={date}/>
+      <Footer date={date}/>
     </div>
   );
 }
---
+++ flower/src/components/Petal.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+function Petal(props){
+    //const [seeded, setSeeded] = useState(props.seeded);
+
+    const petalStyle = {
+        display:"flex",
+        width: `${props.width}px`,
+        height: `${props.height}px`,
+        marginTop:"100px",
+        borderRadius:"50%",
+        transform:`rotate(${props.rotation}deg)`,
+        transform:"scale(3)",
+        background: props.color,
+        background: `linear-gradient(${-props.rotation}deg, ${props.color} 0%, rgba(255,255,255,1) 100%)`,
+    }
+    
+    return(
+    <div style={petalStyle}></div>
+    );
+}
+
+export default Petal;
