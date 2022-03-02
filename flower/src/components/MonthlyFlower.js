f0a24e7a63cc99a8cee39c817abf233825e27055
Jeanette Vahr Thuesen <jeat@itu.dk>
2022-03-02 15:34:23 +0100

    calendar

--- flower/src/components/MonthlyFlower.js
+++ flower/src/components/MonthlyFlower.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React from "react";
+import Styles from "../Styles.css";
+import { useState } from "react";
+import Calendar from "./Calendar";
+import Details from "./Details";
 
-function MonthlyFlower(){
-    const container = {
-        display:"flex",
-        flexDirection:"column",
-        justifyContent:"center",
-        alignItems:"center",
-        height:"500px",
-        padding:"20px",
-    }
+function MonthlyFlower() {
+  const container = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "500px",
+    padding: "20px",
+  };
+  const [showDetails, setShowDetails] = useState(false);
+  const [data, setData] = useState(null);
 
-    return(
-        <div style={container}>
-        <p>Monthly Flower</p>
-        </div>
-    );
+  const showDetailsHandle = (dayStr) => {
+    setData(dayStr);
+    setShowDetails(true);
+  };
+
+  return (
+    <div style={container}>
+      <br />
+      <h1>Your beautiful life</h1>
+      <Calendar showDetailsHandle={showDetailsHandle} />
+      <br />
+    </div>
+  );
 }
 
 export default MonthlyFlower;
